3d10a23a0c66331dc7f042a0b65d6927e970a159
vadim-hleif <vadzim.hleif@gmail.com>
2021-02-17 00:39:31 +0300

    feat(endpoints_test): new tests with mocking

--- pkg/notifications/endpoint/endpoints_test.go
+++ pkg/notifications/endpoint/endpoints_test.go
@@ -1,70 +1,39 @@
 package endpoint
 
 import (
-	"reflect"
+	"github.com/stretchr/testify/assert"
 	"testing"
-
-	"github.com/go-kit/kit/endpoint"
-
-	"simple-tcp-server/pkg/notifications"
 )
 
-func TestMakeEndpoints(t *testing.T) {
-	type args struct {
-		storage notifications.UsersStorage
-	}
-	tests := []struct {
-		name string
-		args args
-		want Endpoints
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := MakeEndpoints(tt.args.storage); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("MakeEndpoints() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+type userStorageMock struct {
+	lastSavedUserId     int
+	lastSavedFriendsIds []int
+
+	mockedFriends []int
+}
+
+func (u *userStorageMock) SaveUser(userId int, friends []int) {
+	u.lastSavedUserId = userId
+	u.lastSavedFriendsIds = friends
+}
+
+func (u *userStorageMock) GetAllFriends(userId int) ([]int, error) {
+	return u.mockedFriends, nil
 }
 
 func Test_makeUserLoginEndpoint(t *testing.T) {
-	type args struct {
-		storage notifications.UsersStorage
-	}
-	tests := []struct {
-		name string
-		args args
-		want endpoint.Endpoint
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := makeUserLoginEndpoint(tt.args.storage); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("makeUserLoginEndpoint() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+	mock := &userStorageMock{}
+	userLoginEndpoint := makeUserLoginEndpoint(mock)
 
-func Test_makeUserLogoutEndpoint(t *testing.T) {
-	type args struct {
-		storage notifications.UsersStorage
-	}
-	tests := []struct {
-		name string
-		args args
-		want endpoint.Endpoint
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := makeUserLogoutEndpoint(tt.args.storage); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("makeUserLogoutEndpoint() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	mock.mockedFriends = []int{2, 4}
+	response, _ := userLoginEndpoint(nil, UserLoginRequest{
+		UserId:     1,
+		FriendsIds: []int{2, 3, 4},
+	})
+	res := response.(UserStatusChangedResponse)
+
+	assert.ElementsMatch(t, mock.mockedFriends, res.OnlineFriendsIds)
+	assert.Equal(t, mock.lastSavedUserId, 1)
+	assert.ElementsMatch(t, mock.lastSavedFriendsIds, []int{2, 3, 4})
+
 }
